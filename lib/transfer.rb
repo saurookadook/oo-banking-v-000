507e3d0d5af11c9ba00d9518ee6bded83b823531
Andy Maskiell <maskiella@gmail.com>
2018-01-04 02:52:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,7 +11,7 @@ class Transfer
   end
 
   def valid?
-    (@sender.valid? && @receiver.valid? && @sender.balance > @amount)
+    (@sender.valid? && @receiver.valid? && @sender.balance > @amount) ? true : false
   end
 
   def execute_transaction
