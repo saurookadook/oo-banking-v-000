8b331091e5bdc75fe1b9c2347d1c1b97cf941563
Andy Maskiell <maskiella@gmail.com>
2018-01-04 02:51:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
+    # binding.pry
     if (@status == "pending" && @sender.valid? && self.valid?)
       @sender.balance -= @amount
       @receiver.balance += @amount
